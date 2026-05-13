a0f821c06994b47f1c7f65303b781c4a595d159e
mhndrachndraa <mhndrachndraa@gmail.com>
2026-05-13 09:42:40 +0700

    Menambahkan override hitungHargaAkhir

---
+++ Elektronik.java
@@ -0,0 +1,16 @@
+public class Elektronik extends Produk {
+
+    private int masaGaransi;
+
+    public Elektronik(String nama, double harga, int stok, int masaGaransi) {
+        super(nama, harga, stok);
+        this.masaGaransi = masaGaransi;
+    }
+
+    @Override
+    public double hitungHargaAkhir(int jmlhBeli) {
+        System.out.println("Total Harga= " + hitungTotalHarga(jmlhBeli));
+        return hitungTotalHarga(jmlhBeli) * 0.85;
+    }
+
+}
